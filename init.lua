caf3d6d9a78ff22d47475bd2a26b1334874a6b8c
TomZz <pythongolangkotlinrust@gmail.com>
2022-11-15 14:23:00 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,34 @@
+-- require("impatient")
+require("utils.global")
+
+-- 基础配置
+require("basic")
+-- 快捷键映射
+require("keybindings")
+-- Packer插件管理
+require("plugins")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+require("plugin-config.toggleterm")
+-- 主题设置
+require("colorscheme")
+-- 自动命令
+require("autocmds")
+-- 内置LSP
+require("lsp.setup")
+-- 自动补全
+require("cmp.setup")
+-- 格式化
+require("format.setup")
+-- DAP
+-- require("dap.setup")
+-- require("dap.vimspector")
+-- utils
+-- 复制到windows剪贴板
+-- require('utils.fix-yank')
---
+++ notes.md
@@ -0,0 +1,22 @@
+# 命令记忆
+## 终端
+### 悬浮终端
+- <leader>ta flaot
+- <leader>tb right
+- <leader>tc below
+### 内置终端
+- st horizontal
+- stv vertical
+## 编辑
+- gd : go to definition
+- gh : display the document
+- gcc/gbc//gc/gb : comment and uncomment
+- zc/zo//zc/Z : fold/unfold
+- = : indent repair(visual)
+- <leader>f : format the whole file
+- <leader>rn : rename the variable
+## Buffer和Window
+- <C-h>/<C-l> : move between buffers
+- <A-h/j/k/l> : move between windows
+- <leader>w/q/qq/wa : write or quit the windows
+- <C-w> : quite the buffer
